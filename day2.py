fbf23d29c110de0476d0c9e781f7d00c360958fe
tazeemsyed <83735670+tazeemsyed@users.noreply.github.com>
2021-05-06 05:25:21 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,79 @@
+#code for changing data type from integer to string using python
+n = 12
+print (type(n),n)
+n = str (n)
+print (type(n),n)
+#code for changing data type from integer to float using python
+n = 12
+print (type(n),n)
+n = float (n)
+print (type(n),n)
+#code for changing data type from integer to boolean using python
+n = 12
+print (type(n),n)
+n = bool (n)
+print (type(n),n)
+
+#code for changing data type from string to int using python
+n = '100'
+print (type(n),n)
+n = int (n)
+print (type(n),n)
+#code for changing data type from string to float using python
+n = '100'
+print (type(n),n)
+n = float (n)
+print (type(n),n)
+#code for changing data type from string to boolean using python
+n = '100'
+print (type(n),n)
+n = bool (n)
+print (type(n),n)
+
+#code for changing data type from float to int using python
+n = 265.65
+print (type(n),n)
+n = int (n)
+print (type(n),n)
+#code for changing data type from float to string using python
+n = 265.65
+print (type(n),n)
+n = str (n)
+print (type(n),n)
+#code for changing data type from float to boolean using python
+n = 265.65
+print (type(n),n)
+n = bool (n)
+print (type(n),n)
+
+#code for changing data type from boolean to int using python
+n = True
+print (type(n),n)
+n = int (n)
+print (type(n),n)
+#code for changing data type from float to string using python
+n = True
+print (type(n),n)
+n = str (n)
+print (type(n),n)
+#code for changing data type from boolean to float using python
+n = True
+print (type(n),n)
+n = float (n)
+print (type(n),n)
+
+#code for changing data type from boolean to int using python
+n = False
+print (type(n),n)
+n = int (n)
+print (type(n),n)
+#code for changing data type from boolean to string using python
+n = False
+print (type(n),n)
+n = str (n)
+print (type(n),n)
+#code for changing data type from boolean to float using python
+n = False
+print (type(n),n)
+n = float (n)
+print (type(n),n)
