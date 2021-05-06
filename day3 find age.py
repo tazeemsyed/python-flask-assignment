85c08b3e6c158cc044001a90193bf8b0dc546780
tazeemsyed <83735670+tazeemsyed@users.noreply.github.com>
2021-05-06 06:09:09 +0530

    Add files via upload

---
+++ day3 find age.py
@@ -0,0 +1,4 @@
+n=input(" What's your name :")
+m= int(input(' Enter your date of birth:'))
+age=2021-m
+print(' Hi', n, '! you are ', age,' years old.')
---
+++ day3simple calculator.py
@@ -0,0 +1,8 @@
+m = int(input('Enter first number : '))
+n = int(input('Enter second number : '))
+print (m,'+' ,n , '=',m+n )
+print (m,'-' ,n , '=',m-n )
+print (m,'x' ,n , '=',m*n )
+print (m,'/' ,n , '=',m/n )
+print (m,'//' ,n , '=',m//n, 'and remainder will be',m%n )
+print (m, 'raise to power', n , 'will be',m ** n )
